f53b4e39d69cb3d1476ea41e15de43318c43043e
Takashi-PC <Takashi-PC@DESKTOP-6970I11>
2019-06-16 14:41:04 +0900

    数表の作成

---
+++ NumberTable/Calc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTable
+{
+    public class Calc
+    {
+        /// <summary>
+        /// 整数 a + b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// 整数 a - b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// 整数 a * b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static long Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// 整数 a ÷ b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static double Division(int a, int b, int digit)
+        {
+            try
+            {
+                return System.Math.Round((double)a / b, digit);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// a の b乗の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static double Power(double a, double b)
+        {
+            try
+            {
+                return System.Math.Pow(a, b);
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        
+        /// <summary>
+        /// aの平方根を返します。
+        /// </summary>
+        public static double Sqrt(double a)
+        {
+            try
+            {
+                return System.Math.Sqrt(a);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
--- NumberTable/Program.cs
+++ NumberTable/Program.cs
@@ -10,6 +10,18 @@ namespace NumberTable
     {
         static void Main(string[] args)
         {
+            Console.Title = "数  表";
+            Console.WriteLine();
+            Console.WriteLine("　数　　二乗　　　三乗　　　四乗　　　平方根");
+            Console.WriteLine(" ----　-------　--------　--------　-----------　");
+            for(int a = 0; a <= 10; a++)
+            {
+                string row = String.Format("{0,2}　　{1,5}　　　{2,5}　　{3,5}　　　　{4:F4}", a, Calc.Power(a, 2), Calc.Power(a, 3), Calc.Power(a, 4), Calc.Sqrt(a));
+                Console.WriteLine(row);
+            }
+            Console.WriteLine();
+            Console.WriteLine("終了するには、何かキーを押してください。");
+            Console.ReadLine();
         }
     }
 }
